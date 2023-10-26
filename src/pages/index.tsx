1c79864cc0a506e578b83e78df4197c22014923f
Djeradé Golbé Parfait <djeradegolbeparfait@gmail.com>
2023-10-26 10:30:39 +0200

    header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,9 +5,13 @@ import { useState } from "react";
 import { Textarea } from "@chakra-ui/react";
 import { DeleteIcon } from "@chakra-ui/icons";
 import { Outlet } from "react-router-dom";
+import logo from 'public/assets/03.png';
+import Image from 'next/image';
+
 
 //Component
 import ListeTask from "@/component/ListTask";
+import { log } from 'console';
 
 
 interface Props {}
@@ -15,9 +19,15 @@ interface Props {}
 
 const Home: NextPage<Props> = ({}) => {
 
-  return <>
-    <Text variant=''> dashboard</Text>
-  </>
+  return <Flex align={'center'} justify={'space-between'}>
+    <Flex p={5} direction={'column'}>
+      <Heading  as='h2' size='lg' >Welcom back !</Heading>
+      <Text color={'gray.400'} fontFamily={theme.fonts.body} >Here's a list of your tasks for this month !</Text>
+    </Flex>
+    <Box mr={5} w={{ base:"35px", sm:"35px", md:"35px", lg:"40px"}}>
+      <Image  src={logo}  alt={'image'}/>
+    </Box>
+  </Flex>
 }
 
 export default Home
